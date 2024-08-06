7b68de942f76589cce29415c09eeddbe921716a3
ganeshmalleedi0774 <ganeshmalleedi0774@gmail.com>
2024-08-06 23:30:01 +0530

    Add project files.

---
+++ MIniKube_Demo2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MIniKube_Demo2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
